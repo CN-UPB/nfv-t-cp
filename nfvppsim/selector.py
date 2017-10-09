cb8f3abcc1f6d0070e3ad8b64f171835328f2ba3
peusterm <manuel.peuster@uni-paderborn.de>
2017-10-09 10:44:03 +0200

    Added UniformGridSelector
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- nfvppsim/selector.py
+++ nfvppsim/selector.py
@@ -28,6 +28,8 @@ LOG = logging.getLogger(os.path.basename(__file__))
 def get_by_name(name):
     if name == "UniformRandomSelector":
         return UniformRandomSelector
+    if name == "UniformGridSelector":
+        return UniformGridSelector
     raise NotImplementedError("'{}' not implemented".format(name))
 
 
@@ -69,6 +71,78 @@ class UniformRandomSelector(object):
 
     def next(self):
         idx = np.random.randint(0, len(self.pm_inputs))
+        self.k_samples += 1
+        return self.pm_inputs[idx]
+
+    def has_next(self):
+        if self.params.get("max_samples") < 0:
+            return True  # -1 infinite samples
+        return (self.k_samples < self.params.get("max_samples", 0))
+
+    def feedback(self, c, r):
+        """
+        Inform selector about result for single configuration.
+        """
+        pass  # TODO store as internal state if needed
+
+    def get_results(self):
+        """
+        Getter for global result collection.
+        :return: dict for result row
+        """
+        r = {"selector": self.short_name,
+             "k_samples": self.k_samples}
+        r.update(self.params)
+        # LOG.debug("Get results from {}: {}".format(self, r))
+        return r
+
+
+class UniformGridSelector(object):
+
+    @classmethod
+    def generate(cls, conf):
+        """
+        Generate list of model objects. One for each conf. to be tested.
+        """
+        r = list()
+        for max_samples in expand_parameters(conf.get("max_samples")):
+            r.append(cls(max_samples=max_samples))
+        return r
+
+    def __init__(self, **kwargs):
+        # apply default params
+        p = {"max_samples": -1}  # -1 infinite samples
+        p.update(kwargs)
+        # members
+        self.pm_inputs = list()
+        self.params = p
+        self.k_samples = 0
+        LOG.debug("Initialized selector: {}".format(self))
+
+    def set_inputs(self, pm_inputs):
+        self.pm_inputs = pm_inputs
+
+    def __repr__(self):
+        return "{}({})".format(self.name, self.params)
+
+    @property
+    def name(self):
+        return self.__class__.__name__
+
+    @property
+    def short_name(self):
+        return re.sub('[^A-Z]', '', self.name)
+
+    def next(self):
+        if self.params.get("max_samples") < 0:
+            LOG.error("{} will not work without positive max_samples setting."
+                      .format(self))
+            LOG.error("Exit!")
+            exit(1)
+        # calculate step size of grind based on size and max_samples
+        step_size = int(len(self.pm_inputs) / self.params.get("max_samples"))
+        # calculate value to be used in this iteration
+        idx = self.k_samples * step_size
         self.k_samples += 1
         return self.pm_inputs[idx]
 
