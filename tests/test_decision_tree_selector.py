b58f6984e87383ace26a438770ca7320cbde6088
Heidi <h.neuhaeuser3@gmail.com>
2019-03-14 11:06:22 +0200

    added test files

---
+++ tests/test_decision_tree_selector.py
@@ -0,0 +1,6 @@
+import unittest
+from nfvtcp.selector import DecisionTreeSelector
+
+class TestDecisionTreeSelector(unittest.TestCase):
+    def test_initialize(self):
+        pass # Todo
---
+++ tests/test_dtree.py
@@ -0,0 +1,9 @@
+import unittest
+from nfvtcp.dtree import DTree
+
+
+class TestDTree(unittest.TestCase):
+    # Todo
+
+    def test_initialize(self):
+        pass
